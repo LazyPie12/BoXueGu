02671b4c5d03975b25e8137b44efa29327597fa6
LazyPie12 <541343944@qq.com>
2019-12-15 17:15:02 +0800

    modify MainActivity.class

--- app/src/main/java/com/example/boxuegu_zsc/activity/MainActivity.java
+++ app/src/main/java/com/example/boxuegu_zsc/activity/MainActivity.java
@@ -2,15 +2,212 @@ package com.example.boxuegu_zsc.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.boxuegu_zsc.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FrameLayout mbodyLayout;  // 中间内容栏
+    private LinearLayout mBottomLayout; // 底部按钮栏
+    private View mCourseBtn;
+    private View mExercisesBtn;
+    private View mMyInfoBtn;
+    private TextView tv_course;
+    private TextView tv_exercises;
+    private TextView tv_myInfo;
+    private ImageView iv_course;
+    private ImageView iv_exercises;
+    private ImageView iv_myInfo;
+    private TextView tv_back;
+    private TextView tv_main_title;
+    private RelativeLayout rl_title_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+        initBottomBar();
+        setListener();
+        setInitStatus();
+    }
+
+    private void init() {
+        tv_back = (TextView) findViewById(R.id.tv_back);
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("博学谷课程");
+        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
+        tv_back.setVisibility(View.GONE);
+        initBodyLayout();
+    }
+
+    private void initBodyLayout() {
+        mbodyLayout = (FrameLayout) findViewById(R.id.main_body);
+    }
+
+    private void initBottomBar() {
+        mBottomLayout = (LinearLayout) findViewById(R.id.main_bottom_bar);
+        mCourseBtn = findViewById(R.id.bottom_bar_course_btn);
+        mExercisesBtn = findViewById(R.id.bottom_bar_exercises_btn);
+        mMyInfoBtn = findViewById(R.id.bottom_bar_myinfo_btn);
+        tv_course = (TextView) findViewById(R.id.bottom_bar_text_course);
+        tv_exercises = (TextView) findViewById(R.id.bottom_bar_text_exercises);
+        tv_myInfo = (TextView) findViewById(R.id.bottom_bar_text_myinfo);
+        iv_course = (ImageView) findViewById(R.id.bottom_bar_image_course);
+        iv_exercises = (ImageView) findViewById(R.id.bottom_bar_image_exercises);
+        iv_myInfo = (ImageView) findViewById(R.id.bottom_bar_image_myinfo);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bottom_bar_course_btn:
+                clearBottomImageState();
+                selectDisplayView(0);
+                break;
+            case R.id.bottom_bar_exercises_btn:
+                clearBottomImageState();
+                selectDisplayView(1);
+                break;
+            case R.id.bottom_bar_myinfo_btn:
+                clearBottomImageState();
+                selectDisplayView(2);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setListener() {
+        for (int i = 0; i < mBottomLayout.getChildCount(); i++) {
+            mBottomLayout.getChildAt(i).setOnClickListener(this);
+        }
+    }
+
+    // 清除底部按钮的选中状态
+    private void clearBottomImageState() {
+        tv_course.setTextColor(Color.parseColor("#666666"));
+        tv_exercises.setTextColor(Color.parseColor("#666666"));
+        tv_myInfo.setTextColor(Color.parseColor("#666666"));
+        iv_course.setImageResource(R.drawable.main_course_icon);
+        iv_exercises.setImageResource(R.drawable.main_exercises_icon);
+        iv_course.setImageResource(R.drawable.main_my_icon);
+        for (int i = 0; i < mBottomLayout.getChildCount(); i++) {
+            mBottomLayout.getChildAt(i).setSelected(false);
+        }
+    }
+
+    // 设置底部按钮的选中状态
+    private void setSelectedStatus(int index) {
+        switch (index) {
+            case 0:
+                mCourseBtn.setSelected(true);
+                iv_course.setImageResource(R.drawable.main_course_icon_selected);
+                tv_course.setTextColor(Color.parseColor("#0097F7"));
+                rl_title_bar.setVisibility(View.VISIBLE);
+                tv_main_title.setText("博学谷课程");
+                break;
+            case 1:
+                mExercisesBtn.setSelected(true);
+                iv_exercises.setImageResource(R.drawable.main_exercises_icon_selected);
+                tv_exercises.setTextColor(Color.parseColor("#0097F7"));
+                rl_title_bar.setVisibility(View.VISIBLE);
+                tv_main_title.setText("博学谷习题");
+                break;
+            case 2:
+                mMyInfoBtn.setSelected(true);
+                iv_myInfo.setImageResource(R.drawable.main_my_icon_selected);
+                tv_myInfo.setTextColor(Color.parseColor("#0097F7"));
+                rl_title_bar.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+    // 移除不需要的视图
+    private void removeAllView() {
+        for (int i = 0; i < mbodyLayout.getChildCount(); i++) {
+            mbodyLayout.getChildAt(i).setVisibility(View.GONE);
+        }
+    }
+
+    // 设置界面view的初始化状态
+    private void setInitStatus() {
+        clearBottomImageState();
+        setSelectedStatus(0);
+        createView(0);
+    }
+
+    // 显示对应的页面
+    private void selectDisplayView(int index) {
+        removeAllView();
+        createView(index);
+        setSelectedStatus(index);
+    }
+
+    // 选择试图
+    private void createView(int viewIndex) {
+        switch (viewIndex) {
+            case 0:
+                // Course
+                break;
+            case 1:
+                // Exercises
+                break;
+            case 2:
+                // MyInfo
+                break;
+        }
+    }
+
+    protected long exitTime;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(MainActivity.this, "再按一次退出博学谷",
+                        Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                MainActivity.this.finish();
+                if (readLoginStatus()) {
+                    clearLoginStatus();
+                }
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    // 获取SharedPreferences中的登录状态
+    private boolean readLoginStatus() {
+        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        boolean isLogin = sp.getBoolean("isLogin", false);
+        return isLogin;
+    }
+
+    // 清除SharedPreferences中的登录状态
+    private void clearLoginStatus() {
+        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isLogin", false);
+        editor.putString("loginUserName", "");
+        editor.commit();
     }
 }
--- app/src/main/java/com/example/boxuegu_zsc/activity/SplashActivity.java
+++ app/src/main/java/com/example/boxuegu_zsc/activity/SplashActivity.java
@@ -41,7 +41,7 @@ public class SplashActivity extends AppCompatActivity {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 SplashActivity.this.finish();
             }
